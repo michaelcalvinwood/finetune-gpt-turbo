b6440f36297893407bde0cb198548ba4e2e82e0c
Michael Wood <michaelwood33311@icloud.com>
2024-03-03 08:12:54 -0500

    added mysql

--- finetune-gpt-turbo.js
+++ finetune-gpt-turbo.js
@@ -1,5 +1,8 @@
 
 const axios = require('axios');
+const papa = require("papaparse");
+
+const mysql = require('./utils/mysql');
 
 const dataUrl = `https://www.michaelcalvinwood.net/datasets/text-data/NewsArticles.csv`;
 
@@ -13,12 +16,27 @@ async function getCsv (url) {
     }
 }
 
+function convertCsvToOutput (csv) {
+    const parsed = papa.parse(csv).data;
+    console.log('parsed', parsed);
+    for (let i = 0; i < parsed.length; ++i) {
+        const entry = parsed[i];
+        const text = entry.replaceAll()
+        console.log(entry[5]);
+        if (i > 3) break;
+    }
+}
+
 
 
 
 
 const test = async () => {
+    const rows = await mysql.query('SHOW DATABASES');
+    console.log(rows);
+    return;
+
     const csv = await getCsv(dataUrl);
-    console.log(csv.substring(0, 512));
+    convertCsvToOutput(csv);
 }
 test();
