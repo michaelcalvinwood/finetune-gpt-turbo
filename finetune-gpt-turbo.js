b732408ddfba074a896708c3b9740cc98c7c609e
Michael Wood <michaelwood33311@icloud.com>
2024-03-03 07:49:13 -0500

    added fetching csv

--- finetune-gpt-turbo.js
+++ finetune-gpt-turbo.js
@@ -1,2 +1,24 @@
-const dataUrl = `https://michaelcalvinwood.net/datasets/text-data/NewsArticles.csv`;
 
+const axios = require('axios');
+
+const dataUrl = `https://www.michaelcalvinwood.net/datasets/text-data/NewsArticles.csv`;
+
+async function getCsv (url) {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (err) {
+        console.error(err);
+        return '';
+    }
+}
+
+
+
+
+
+const test = async () => {
+    const csv = await getCsv(dataUrl);
+    console.log(csv.substring(0, 512));
+}
+test();
